ad085eda9b036fa8b3f24190176434a9caf85c36
emushtaq <eshanmushtaq@gmail.com>
2016-08-17 12:58:28 +0200

    added map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,45 @@
  * Controller of the feApp
  */
 angular.module('feApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyApmA-heaA7NVO6GYfQI1LiL_zyDAQvSok',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
+  .controller('MainCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function ($scope, $http, uiGmapGoogleMapApi) {
+    $scope.user = {
+      fromXCoordinates : '52.52656141115588',
+      fromYCoordinates : '13.358001708984375',
+      toXCoordinates : '52.493233155027156',
+      toYCoordinates : '13.422374725341797',
+      date : '10-20-2015',
+      time : '05:13:00pm',
+      maxWalkDistance : 800 };
+
+    $scope.fetchRoutes = function() {
+      let urlData = 'http://localhost:8080/itineraries?' +
+      'fromX='+$scope.user.fromXCoordinates+'&' +
+      'fromY='+$scope.user.fromYCoordinates+'&'+
+      'toX='+$scope.user.toXCoordinates+'&'+
+      'toY='+$scope.user.toYCoordinates+'&'+
+      'journeyTime='+$scope.user.time+'&'+
+      'journeyDate='+$scope.user.date;
+
+      $http({
+          method: 'GET',
+          url: urlData
+        }).then(function successCallback(response) {
+            $scope.response = response.data;
+          }, function errorCallback(response) {
+            $scope.response = response;
+          });
+      };
+
+      $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+      uiGmapGoogleMapApi.then(function(maps) {
+        console.log(maps);
+    });
+  }]);
